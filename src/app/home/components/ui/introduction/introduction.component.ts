8e92f3966b9b728ed10d4c571b5ae622815885a7
Elea641 <eleavoliotis64@gmail.com>
2024-08-08 13:58:33 +0200

    wip introduction animation

--- src/app/home/components/ui/introduction/introduction.component.ts
+++ src/app/home/components/ui/introduction/introduction.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,13 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './introduction.component.html',
-  styleUrls: ['./introduction.component.scss']
+  styleUrls: ['./introduction.component.scss'],
 })
-export class IntroductionComponent {
-
+export class IntroductionComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const h2Element = document.querySelector('h2') as HTMLElement;
+    if (h2Element) {
+      h2Element.style.width = `${h2Element.scrollWidth}px`;
+    }
+  }
 }
--- src/app/home/components/ui/training/training.component.ts
+++ src/app/home/components/ui/training/training.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingCardComponent } from '../training-card/training-card.component';
 import { Training } from 'src/app/home/models/training.type';
@@ -10,6 +10,58 @@ import { Training } from 'src/app/home/models/training.type';
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.scss'],
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
   @Input() trainings!: Training[];
+  private sectionObserver: IntersectionObserver;
+  private cardObserver: IntersectionObserver | null = null;
+
+  constructor() {
+    this.sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.observeCards();
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    const sectionElement = document.querySelector('#training');
+    if (sectionElement) {
+      this.sectionObserver.observe(sectionElement);
+    }
+  }
+
+  observeCards(): void {
+    this.cardObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const element = entry.target as HTMLElement;
+          const index = Array.from(element.parentNode!.children).indexOf(
+            element
+          );
+
+          if (entry.isIntersecting) {
+            element.classList.add('in-view');
+            element.classList.remove('out-of-view');
+            element.style.transitionDelay = `${index * 0.2}s`;
+          } else {
+            element.classList.add('out-of-view');
+            element.classList.remove('in-view');
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    const trainingCards = document.querySelectorAll('ul > li');
+    trainingCards.forEach((card) => this.cardObserver!.observe(card));
+  }
 }
