e0af94b802cb87f1ffb9603c233677e93c4f48c6
Elea641 <(mailto:eleavoliotis64@gmail.com)>
2024-09-24 15:51:28 +0200

    add copyright

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntroductionComponent } from '../../components/ui/introduction/introduction.component';
 import { PresentationComponent } from '../../components/ui/presentation/presentation.component';
@@ -26,38 +26,4 @@ import { TrainingDataComponent } from '../../components/feature/training-data/tr
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements AfterViewInit {
-  isVisible: boolean = false;
-
-  ngAfterViewInit(): void {
-    const scrollToTopButton = document.getElementById('scroll-to-top');
-    const scrollButtonImage = document.getElementById(
-      'scroll-button-image'
-    ) as HTMLImageElement;
-
-    if (scrollToTopButton && scrollButtonImage) {
-      const changeImageAtPixels = 3.5 * 16;
-      const viewportHeight = window.innerHeight;
-      const changeImageAt150vh = (107 * viewportHeight) / 100;
-
-      window.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition > changeImageAt150vh) {
-          scrollButtonImage.src = 'assets/pictures/logo-arrow-up.png';
-          this.isVisible = true;
-        } else if (scrollPosition > changeImageAtPixels) {
-          scrollButtonImage.src = 'assets/pictures/logo-arrow-up-gray.png';
-          this.isVisible = true;
-        } else {
-          scrollButtonImage.src = 'assets/pictures/logo-arrow-up.png';
-          this.isVisible = false;
-        }
-      });
-
-      scrollToTopButton.addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      });
-    }
-  }
-}
+export class HomePageComponent {}
