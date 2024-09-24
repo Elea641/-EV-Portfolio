195fea13edf0b8eb43714d0377a74830bff52807
Elea641 <(mailto:eleavoliotis64@gmail.com)>
2024-09-24 14:54:40 +0200

    fixe style card project

--- src/app/home/models/project.type.ts
+++ src/app/home/models/project.type.ts
@@ -1,8 +1,11 @@
 export type Project = {
   id: number;
   name: string;
+  trainingCenter: string,
   url: string;
+  date: string;
   type: string;
   technos: string;
   github_url: string;
+  content: string;
 };
