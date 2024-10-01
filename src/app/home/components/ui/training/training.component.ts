10b47436fa4b15a8cbd480ea7fc68a7592308431
Elea641 <eleavoliotis64@gmail.com>
2024-10-01 15:39:24 +0200

    wip form contact

--- src/app/home/components/ui/training/training.component.ts
+++ src/app/home/components/ui/training/training.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingCardComponent } from '../training-card/training-card.component';
 import { Training } from 'src/app/home/models/training.type';
