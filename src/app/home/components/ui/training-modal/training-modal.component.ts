548908b4ccee9878407ffe8aa6f7d522039e0659
Elea641 <eleavoliotis64@gmail.com>
2024-10-03 15:44:59 +0200

    add style responsive

--- src/app/home/components/ui/training-modal/training-modal.component.ts
+++ src/app/home/components/ui/training-modal/training-modal.component.ts
@@ -12,20 +12,7 @@ import { Training } from 'src/app/home/models/training.type';
 export class TrainingModalComponent {
   @Input() training: Training | null = null;
 
-  // Ajouter la classe 'modal-open' et 'no-scroll' au body lors de l'ouverture
-  ngOnInit() {
-    document.querySelector('.modal')?.classList.add('modal-open');
-    document.body.classList.add('no-scroll'); // Empêche le scroll en arrière-plan
-  }
-
-  // Enlever les classes 'modal-open' et 'no-scroll' lors de la fermeture
   closeModal() {
     document.querySelector('.modal')?.classList.remove('modal-open');
-    document.body.classList.remove('no-scroll'); // Réactiver le scroll en arrière-plan
-  }
-
-  // Nettoyer quand le composant est détruit (quand la modal se ferme)
-  ngOnDestroy() {
-    this.closeModal(); // S'assurer que les classes sont bien supprimées
   }
 }
