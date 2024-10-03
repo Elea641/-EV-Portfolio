ab876c8fb2ce71a2a319f86f698b0bccea81cb6c
Elea641 <eleavoliotis64@gmail.com>
2024-10-03 11:42:57 +0200

    test deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  EMAIL_SERVICE_ID: '',
-  EMAIL_TEMPLATE_ID: '',
-  EMAIL_USER_ID: '',
+  EMAIL_SERVICE_ID: 'service_v1h3ekk',
+  EMAIL_TEMPLATE_ID: 'template_gs4x49o',
+  EMAIL_USER_ID: 'Rzeq3K0MvRz40XWzA',
 };
