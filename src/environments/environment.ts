c0f0bb1b017074dab03d2a0731dc2e14c766ed63
Elea641 <eleavoliotis64@gmail.com>
2024-10-03 11:52:10 +0200

    test deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,6 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 export const environment = {
   production: false,
-  EMAIL_SERVICE_ID: process.env['EMAIL_SERVICE_ID'],
-  EMAIL_TEMPLATE_ID: process.env['EMAIL_TEMPLATE_ID'],
-  EMAIL_USER_ID: process.env['EMAIL_USER_ID'],
+  EMAIL_SERVICE_ID: process.env['EMAIL_SERVICE_ID'] || '',
+  EMAIL_TEMPLATE_ID: process.env['EMAIL_TEMPLATE_ID'] || '',
+  EMAIL_USER_ID: process.env['EMAIL_USER_ID'] || '',
 };
