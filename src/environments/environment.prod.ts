8712ceff86fdb57db6b2ed3038fd4a0bb053e1bb
Elea641 <eleavoliotis64@gmail.com>
2024-10-03 13:44:52 +0200

    test deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  EMAIL_SERVICE_ID: '',
-  EMAIL_TEMPLATE_ID: '',
-  EMAIL_USER_ID: '',
+  EMAIL_SERVICE_ID: import.meta.env.NG_EMAIL_SERVICE_ID,
+  EMAIL_TEMPLATE_ID: import.meta.env.NG_EMAIL_TEMPLATE_ID,
+  EMAIL_USER_ID: import.meta.env.NG_EMAIL_USER_ID,
 };
