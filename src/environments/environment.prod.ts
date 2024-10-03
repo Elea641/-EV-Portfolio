01517e1d5e36d619b7834440794acd67ec726d74
Elea641 <eleavoliotis64@gmail.com>
2024-10-03 11:55:16 +0200

    test deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  EMAIL_SERVICE_ID: process.env['EMAIL_SERVICE_ID'] || '',
-  EMAIL_TEMPLATE_ID: process.env['EMAIL_TEMPLATE_ID'] || '',
-  EMAIL_USER_ID: process.env['EMAIL_USER_ID'] || '',
+  EMAIL_SERVICE_ID: 'EMAIL_SERVICE_ID',
+  EMAIL_TEMPLATE_ID: 'EMAIL_TEMPLATE_ID',
+  EMAIL_USER_ID: 'EMAIL_USER_ID',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  EMAIL_SERVICE_ID: process.env['EMAIL_SERVICE_ID'] || '',
-  EMAIL_TEMPLATE_ID: process.env['EMAIL_TEMPLATE_ID'] || '',
-  EMAIL_USER_ID: process.env['EMAIL_USER_ID'] || '',
+  EMAIL_SERVICE_ID: 'EMAIL_SERVICE_ID',
+  EMAIL_TEMPLATE_ID: 'EMAIL_TEMPLATE_ID',
+  EMAIL_USER_ID: 'EMAIL_USER_ID',
 };
