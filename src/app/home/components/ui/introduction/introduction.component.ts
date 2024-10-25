6061ac64e4c85f393f0e87f2987489de7d07760e
Elea641 <eleavoliotis64@gmail.com>
2024-10-25 15:11:46 +0200

    aad projects pictures

--- src/app/home/components/ui/introduction/introduction.component.ts
+++ src/app/home/components/ui/introduction/introduction.component.ts
@@ -8,11 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.scss'],
 })
-export class IntroductionComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    const h2Element = document.querySelector('h2') as HTMLElement;
-    if (h2Element) {
-      h2Element.style.width = `${h2Element.scrollWidth}px`;
-    }
-  }
-}
+export class IntroductionComponent  {}
