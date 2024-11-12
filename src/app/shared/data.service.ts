34ba1a484114582fae4e0dbba09c1321f54a378c
Elea641 <eleavoliotis64@gmail.com>
2024-11-12 14:36:02 +0100

    add project page

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService<T> {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(url: string): Observable<T[]> {
+    return this.http.get<T[]>(url);
+  }
+}
