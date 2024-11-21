37683992d82e8984bd1d4b890cc0b153923d115f
Elea641 <eleavoliotis64@gmail.com>
2024-11-21 10:28:27 +0100

    fix color dot theme

--- src/app/project/components/ui/project-details-card/project-details-card.component.ts
+++ src/app/project/components/ui/project-details-card/project-details-card.component.ts
@@ -1,15 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Project } from 'src/app/home/models/project.type';
 import { CarouselProjectComponent } from '../carousel-project/carousel-project.component';
+import { RouterModule } from '@angular/router';
+import { ThemeUtilService } from 'src/app/home/shared/theme.util.service';
 
 @Component({
   selector: 'app-project-details-card',
   standalone: true,
-  imports: [CommonModule, CarouselProjectComponent],
+  imports: [CommonModule, CarouselProjectComponent, RouterModule],
   templateUrl: './project-details-card.component.html',
   styleUrls: ['./project-details-card.component.scss'],
 })
-export class ProjectDetailsCardComponent {
+export class ProjectDetailsCardComponent implements OnInit {
   @Input() project!: Project;
+  isDarkTheme = false;
+
+  constructor(
+    public themeUtilService: ThemeUtilService,
+  ) { }
+
+  ngOnInit() {
+    const themeChoice = localStorage.getItem('theme');
+    if (!themeChoice || themeChoice === '') {
+      this.isDarkTheme = true;
+      localStorage.setItem('theme', '');
+    } else {
+      this.isDarkTheme = themeChoice === 'ligth-theme';
+    }
+
+    this.themeUtilService.isLightThemeChanged$.subscribe(isLight => {
+      this.isDarkTheme = !isLight;
+    });
+  }
 }
